1c8d9ced15b921ddc8be6d57688e74da701d0244
meriamdref2000 <84708813+meriamdref2000@users.noreply.github.com>
2022-12-26 00:16:36 +0100

    commit

--- front-end/src/app/models/over-speed.ts
+++
@@ -1,6 +0,0 @@
-export class OverSpeed {
-  overSpeedId?: string;
-  timeStamp?: string;
-  vehicleRegistrationNumber?: number;
-  vehicleSpeed?: number;
-}
--- front-end/src/app/models/owner.model.ts
+++
@@ -1,8 +0,0 @@
-export class Owner {
-  id?: string;
-  ownerName?: string;
-  ownerNationalIdCard?: string;
-  ownerEmail?: string;
-  ownerPhoneNumber?: string;
-  ownerAddress?: string;
-}
